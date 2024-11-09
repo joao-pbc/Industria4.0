a5193a3fdb19c13a600418bc225d5aaeea967c4b
VtrMoura <147671100+VtrMoura@users.noreply.github.com>
2024-11-08 21:05:08 -0300

    aula0801

--- js/script.js
+++ js/script.js
@@ -1,21 +1,19 @@
-google.charts.load("current", { packages: ["corechart"] });
+google.charts.load('current', { packages: ['table'] });
 google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['Work', 11],
-        ['Eat', 2],
-        ['Commute', 2],
-        ['Watch TV', 2],
-        ['Sleep', 7]
+function drawTable() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Name');
+    data.addColumn('number', 'Salary');
+    data.addColumn('boolean', 'Full Time Employee');
+    data.addRows([
+        ['Mike', { v: 10000, f: '$10,000' }, true],
+        ['Jim', { v: 8000, f: '$8,000' }, false],
+        ['Alice', { v: 12500, f: '$12,500' }, true],
+        ['Bob', { v: 7000, f: '$7,000' }, true]
     ]);
 
-    var options = {
-        title: 'My Daily Activities',
-        is3D: true,
-    };
+    var table = new google.visualization.Table(document.getElementById('div2'));
 
-    var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-    chart.draw(data, options);
-}   
+    table.draw(data, { showRowNumber: true, width: '100%', height: '100%' });
+}
