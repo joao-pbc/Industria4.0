325959ff918f5b067c04ed286dd95a1bd73c2353
JOÃO PEDRO BOVE DA COSTA <joao_b_costa@estudante.sesisenai.org.br>
2024-11-21 21:17:43 -0300

    Tabela puxando dados da api

--- js/script.js
+++ js/script.js
@@ -1,25 +1,13 @@
 google.charts.load('current', {'packages':['table']});
 google.charts.setOnLoadCallback(drawTable);
 
-function drawTable(data) {
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Nome');
-  data.addColumn('number', 'Quantidade total');
-   data.addRows([
-    ['Peça Vermelha',  {v: 10000, f: '10 unidades'}, ],
-    ['Peça Azul',   {v:8000,   f: '15 unidades'},  ],
-    ['Peça Preta', {v: 12500, f: '11 unidades'}, ],
-    ['Peça Amarela',   {v: 7000,  f: '3 unidades'},  ]
-  ]);
-
+async function drawTable() {
+  var data = await drawPieChart();
   var table = new google.visualization.Table(document.getElementById('div2'));
-
-
   var options = {
     width: "100%",
     height: "100%",
     backgroundColor: "#6A6868",
   };
-
-  table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+  table.draw(data, options);
 }
--- script/handler.js
+++ script/handler.js
@@ -1,57 +1,27 @@
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
+// Set a callback to run when the Google Visualization API is loaded.
 
-google.charts.load("current", {packages:["corechart"]});
-google.charts.setOnLoadCallback(drawChart);
-const url = 'http://localhost:5678/api/Products';
-
-function connect(){
-    fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json(); // Parse the JSON from the response
-    })
-    .then(data => {
-      return data; // Handle the data from the response
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
-    });
+async function connect(url){
+ const respone = await fetch(url);
+ return await respone.json();
 }
 
-document.addEventListener('onload', () => {
-    drawChart();
-})
+async function drawPieChart() {
+  const url = 'http://10.3.17.186:5678/api/Inventory';
+  const data = await connect(url);
+  var dataTable = getDataTableForTable(data);
+  return dataTable;
+}
 
-
-function drawChart() {
-
-    var data = connect();
-
-    // Converter o JSON para o formato do Google Charts
-    var dataArray = [['Label', 'Batch Size']]; // Cabeçalho das colunas
-
-    // Preencher os dados a partir do JSON
-    data.forEach(function(item) {
-    dataArray.push([item.label, item.batchSize]);
-    });
-
-    // Converter dataArray para um DataTable do Google Charts
-    var dataTable = google.visualization.arrayToDataTable(dataArray);
-
-    var options = {
-        title: 'Batch Size por Label',
-        chartArea: { width: '50%' },
-        hAxis: {
-          title: 'Batch Size',
-          minValue: 0
-        },
-        vAxis: {
-          title: 'Label'
-        }
-      };
-
-      var chart = new google.visualization.BarChart(document.getElementById("div3"));
-      chart.draw(dataTable, options);
- 
+function getDataTableForTable(jsonObject) {
+  dataTable = new google.visualization.DataTable();
+  dataTable.addColumn('string', 'Nome');
+  dataTable.addColumn('number', 'Quantidade total');
+  jsonObject.productsInventory.forEach(product => {
+    dataTable.addRow(
+      [product.productCode,  {v: product.quantity}]
+    )
+  });
+  return dataTable;
 }
