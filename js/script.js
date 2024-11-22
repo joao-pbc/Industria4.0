7ce8def644b25d2b0c7f31e3aa0ce8866b43464e
VICTOR SOARES MOURA <victor_s_moura@estudante.sesisenai.org.br>
2024-11-21 21:16:44 -0300

    aula2111

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
 google.charts.load('current', {'packages':['table']});
 google.charts.setOnLoadCallback(drawTable);
 
-function drawTable(data) {
+function drawTable() {
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'Nome');
   data.addColumn('number', 'Quantidade total');
-   data.addRows([
-    ['Peça Vermelha',  {v: 10000, f: '10 unidades'}, ],
-    ['Peça Azul',   {v:8000,   f: '15 unidades'},  ],
-    ['Peça Preta', {v: 12500, f: '11 unidades'}, ],
-    ['Peça Amarela',   {v: 7000,  f: '3 unidades'},  ]
+  data.addRows([
+    ['Peça Vermelha', {v: 10000, f: '10 unidades'}],
+    ['Peça Azul', {v: 8000, f: '15 unidades'}],
+    ['Peça Preta', {v: 12500, f: '11 unidades'}],
+    ['Peça Amarela', {v: 7000, f: '3 unidades'}]
   ]);
 
   var table = new google.visualization.Table(document.getElementById('div2'));
 
-
   var options = {
     width: "100%",
     height: "100%",
-    backgroundColor: "#6A6868",
+    allowHtml: true
   };
 
-  table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+  table.draw(data, options);
 }
--- script/handler.js
+++ script/handler.js
@@ -1,57 +1,57 @@
 
-google.charts.load("current", {packages:["corechart"]});
-google.charts.setOnLoadCallback(drawChart);
-const url = 'http://localhost:5678/api/Products';
+// google.charts.load("current", {packages:["corechart"]});
+// google.charts.setOnLoadCallback(drawChart);
+// const url = 'http://localhost:5678/api/Products';
 
-function connect(){
-    fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json(); // Parse the JSON from the response
-    })
-    .then(data => {
-      return data; // Handle the data from the response
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
-    });
-}
+// function connect(){
+//     fetch(url)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+//       return response.json(); // Parse the JSON from the response
+//     })
+//     .then(data => {
+//       return data; // Handle the data from the response
+//     })
+//     .catch(error => {
+//       console.error('There was a problem with the fetch operation:', error);
+//     });
+// }
 
-document.addEventListener('onload', () => {
-    drawChart();
-})
+// document.addEventListener('onload', () => {
+//     drawChart();
+// })
 
 
-function drawChart() {
+// function drawChart() {
 
-    var data = connect();
+//     var data = connect();
 
-    // Converter o JSON para o formato do Google Charts
-    var dataArray = [['Label', 'Batch Size']]; // Cabeçalho das colunas
+//     // Converter o JSON para o formato do Google Charts
+//     var dataArray = [['Label', 'Batch Size']]; // Cabeçalho das colunas
 
-    // Preencher os dados a partir do JSON
-    data.forEach(function(item) {
-    dataArray.push([item.label, item.batchSize]);
-    });
+//     // Preencher os dados a partir do JSON
+//     data.forEach(function(item) {
+//     dataArray.push([item.label, item.batchSize]);
+//     });
 
-    // Converter dataArray para um DataTable do Google Charts
-    var dataTable = google.visualization.arrayToDataTable(dataArray);
+//     // Converter dataArray para um DataTable do Google Charts
+//     var dataTable = google.visualization.arrayToDataTable(dataArray);
 
-    var options = {
-        title: 'Batch Size por Label',
-        chartArea: { width: '50%' },
-        hAxis: {
-          title: 'Batch Size',
-          minValue: 0
-        },
-        vAxis: {
-          title: 'Label'
-        }
-      };
+//     var options = {
+//         title: 'Batch Size por Label',
+//         chartArea: { width: '50%' },
+//         hAxis: {
+//           title: 'Batch Size',
+//           minValue: 0
+//         },
+//         vAxis: {
+//           title: 'Label'
+//         }
+//       };
 
-      var chart = new google.visualization.BarChart(document.getElementById("div3"));
-      chart.draw(dataTable, options);
+//       var chart = new google.visualization.BarChart(document.getElementById("div3"));
+//       chart.draw(dataTable, options);
  
-}
+// }
