3951faf25b5508e3488f4d3e2e932e0c285ede0f
JOÃO PEDRO BOVE DA COSTA <joao_b_costa@estudante.sesisenai.org.br>
2024-11-26 18:56:49 -0300

    Resolvendo conflito

--- script/handler.js
+++ script/handler.js
@@ -2,7 +2,6 @@
 google.charts.load('current', {'packages':['corechart']});
 // Set a callback to run when the Google Visualization API is loaded.
 
-<<<<<<< HEAD
 async function connect(url){
  const respone = await fetch(url);
  return await respone.json();
@@ -26,61 +25,3 @@ function getDataTableForTable(jsonObject) {
   });
   return dataTable;
 }
-=======
-// google.charts.load("current", {packages:["corechart"]});
-// google.charts.setOnLoadCallback(drawChart);
-// const url = 'http://localhost:5678/api/Products';
-
-// function connect(){
-//     fetch(url)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//       }
-//       return response.json(); // Parse the JSON from the response
-//     })
-//     .then(data => {
-//       return data; // Handle the data from the response
-//     })
-//     .catch(error => {
-//       console.error('There was a problem with the fetch operation:', error);
-//     });
-// }
-
-// document.addEventListener('onload', () => {
-//     drawChart();
-// })
-
-
-// function drawChart() {
-
-//     var data = connect();
-
-//     // Converter o JSON para o formato do Google Charts
-//     var dataArray = [['Label', 'Batch Size']]; // Cabeçalho das colunas
-
-//     // Preencher os dados a partir do JSON
-//     data.forEach(function(item) {
-//     dataArray.push([item.label, item.batchSize]);
-//     });
-
-//     // Converter dataArray para um DataTable do Google Charts
-//     var dataTable = google.visualization.arrayToDataTable(dataArray);
-
-//     var options = {
-//         title: 'Batch Size por Label',
-//         chartArea: { width: '50%' },
-//         hAxis: {
-//           title: 'Batch Size',
-//           minValue: 0
-//         },
-//         vAxis: {
-//           title: 'Label'
-//         }
-//       };
-
-//       var chart = new google.visualization.BarChart(document.getElementById("div3"));
-//       chart.draw(dataTable, options);
- 
-// }
->>>>>>> 7ce8def644b25d2b0c7f31e3aa0ce8866b43464e
