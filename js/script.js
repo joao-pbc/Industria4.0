bba9f0f0f9cd9430c57be33c0a1d00e14743b0ce
JOÃO PEDRO BOVE DA COSTA <joao_b_costa@estudante.sesisenai.org.br>
2024-11-26 19:33:49 -0300

    Refatorado método que chama a api

--- js/script.js
+++ js/script.js
@@ -2,12 +2,25 @@ google.charts.load('current', {'packages':['table']});
 google.charts.setOnLoadCallback(drawTable);
 
 async function drawTable() {
-  var data = await drawPieChart();
+  var data = await drawPieChart("Inventory");
+  var dataTable = getDataTableForTable(data);
   var table = new google.visualization.Table(document.getElementById('div2'));
   var options = {
     width: "100%",
     height: "100%",
     allowHtml: true
   };
-  table.draw(data, options);
+  table.draw(dataTable, options);
+}
+
+function getDataTableForTable(jsonObject) {
+  dataTable = new google.visualization.DataTable();
+  dataTable.addColumn('string', 'Nome');
+  dataTable.addColumn('number', 'Quantidade total');
+  jsonObject.productsInventory.forEach(product => {
+    dataTable.addRow(
+      [product.productCode,  {v: product.quantity}]
+    )
+  });
+  return dataTable;
 }
--- script/handler.js
+++ script/handler.js
@@ -1,27 +1,13 @@
-// Load the Visualization API and the corechart package.
 google.charts.load('current', {'packages':['corechart']});
-// Set a callback to run when the Google Visualization API is loaded.
 
 async function connect(url){
  const respone = await fetch(url);
  return await respone.json();
 }
 
-async function drawPieChart() {
-  const url = 'http://10.3.17.186:5678/api/Inventory';
-  const data = await connect(url);
-  var dataTable = getDataTableForTable(data);
-  return dataTable;
+function drawPieChart(objeto) {
+  const url = 'http://10.26.92.250:5678/api/' + objeto;
+  const data = connect(url);
+  return data;
 }
 
-function getDataTableForTable(jsonObject) {
-  dataTable = new google.visualization.DataTable();
-  dataTable.addColumn('string', 'Nome');
-  dataTable.addColumn('number', 'Quantidade total');
-  jsonObject.productsInventory.forEach(product => {
-    dataTable.addRow(
-      [product.productCode,  {v: product.quantity}]
-    )
-  });
-  return dataTable;
-}
